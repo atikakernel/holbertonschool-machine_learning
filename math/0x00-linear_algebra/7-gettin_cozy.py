c0942e80c807f748f8a7f7daafb98322f9d267ab
Diego C <777@holbertonschool.com>
2020-01-28 16:47:17 -0500

    append matrices without mutability

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Gettin’ Cozy """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concatenates two matrices along a specific axis """
+    copy1 = [row[:] for row in mat1]
+    copy2 = mat2.copy()
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        return copy1 + copy2
+
+    elif axis == 1 and len(mat1) == len(mat2):
+        for arr1, arr2 in zip(copy1, copy2):
+            arr1.extend(arr2)
+        return copy1
