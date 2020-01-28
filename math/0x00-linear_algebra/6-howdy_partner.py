7f63464bc6600c98dd0fbfd1d89ce2ba0279b7b1
Diego C <777@holbertonschool.com>
2020-01-28 15:14:45 -0500

    concatenate two arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Add a function to calculate the element wise sum of a 2D matrix"""
+
+
+def cat_arrays(arr1, arr2):
+    """calculate element wise sum of a 2D matrix"""
+    return arr1 + arr2
