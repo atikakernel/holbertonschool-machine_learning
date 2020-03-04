199bad84a77ed0005a0d923873defef553d2a33c
diegokernel <777@holbertonschool.com>
2020-03-04 03:55:26 +0000

    style

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -5,7 +5,7 @@
 import tensorflow as tf
 
 
-def create_RMSProp_op(loss, alpha, beta2, epsilon):
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """ creates the training operation for a neural network in
     tensorflow using the Adam optimization algorithm
     """
