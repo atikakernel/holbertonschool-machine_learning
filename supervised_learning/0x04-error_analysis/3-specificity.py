39f4788c2a9d882d2be9c0a2afb1c270470d05ba
diegokernel <777@holbertonschool.com>
2020-03-11 19:57:55 +0000

    fixu

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -6,5 +6,5 @@ import numpy as np
 def specificity(confusion):
     """creates a confusion matrix:"""
     return ((np.sum(confusion) - np.sum(confusion, axis=0) -
-            np.sum(confusion, axis=1) + np.diagonal(confusion)) / 
+             np.sum(confusion, axis=1) + np.diagonal(confusion)) /
             (np.sum(confusion) - np.sum(confusion, axis=1)))
