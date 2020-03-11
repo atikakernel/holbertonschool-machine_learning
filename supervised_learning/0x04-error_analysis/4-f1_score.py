b3791285b92b463a5e21ec010863d81d1167b783
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2020-03-11 14:45:58 -0500

    Update 4-f1_score.py

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def f1_score(confusion):
     """creates a confusion matrix:"""
-    precision = np.diagonal(confusion)/np.sum(confusion, axis=0)
-    sensibility = np.diagonal(confusion)/np.sum(confusion, axis=1)
+    precision = np.diagonal(confusion) / (np.sum(confusion, axis=0))
+    sensibility = np.diagonal(confusion) / (np.sum(confusion, axis=1))
     return (2 * (precision * sensibility) / (precision + sensibility))
