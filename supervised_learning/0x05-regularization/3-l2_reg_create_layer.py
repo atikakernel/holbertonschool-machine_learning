ede467e1a88cb11bd027c4062dbbf2a5d7987259
diegokernel <777@holbertonschool.com>
2020-03-13 01:15:20 +0000

    33

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -6,9 +6,9 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """ calculates the cost of a neural network with L2 regularization: """
     regul = tf.contrib.layers.l2_regularizer(lambtha)
-    raw  = tf.contrib.layers.variance_scaling_initializer(
+    raw = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
     o_tensor = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=raw,
-                            kernel_regularizer=regul)
+                               kernel_initializer=raw,
+                               kernel_regularizer=regul)
     return(o_tensor(prev))
