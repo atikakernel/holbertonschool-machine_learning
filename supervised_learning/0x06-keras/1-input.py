c718a4dfe6cc6950891cbae780f5dd4cede2dbea
diegokernel <777@holbertonschool.com>
2020-03-17 04:14:59 +0000

    re

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -6,10 +6,10 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """ build a neuronal network """
     inputs = K.Input(shape=(nx,))
-    regularizers = K.regularizers.l2(lambtha)
+    reg = K.regularizers.l2(lambtha)
 
     model = K.layers.Dense(layers[0], activation=activations[0],
-                           kernel_regularizer=regularizers)(inputs)
+                           kernel_regularizer=reg)(inputs)
 
     for i in range(1, len(layers)):
         model = K.layers.Dropout(rate=1-keep_prob)(model)
