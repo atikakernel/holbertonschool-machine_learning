677009a2a5e8ecd9618d1f5de9a0dbc75e1f2a3b
Diego <777@holbertonschool.com>
2020-06-25 23:05:44 -0500

    dd

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -16,9 +16,9 @@ def inception_network():
                                 activation='relu',
                                 kernel_initializer=lay_init)
     conv1 = conv_lay1(X)
-    max_pooling2d_1  = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding='same')
-    maxpool1 =  max_pooling2d_1(conv1)
+    max_pooling2d_1 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
+                                            padding='same')
+    maxpool1 = max_pooling2d_1(conv1)
     conv_lay2 = K.layers.Conv2D(filters=64, kernel_size=(1, 1),
                                 activation='relu',
                                 kernel_initializer=lay_init)
@@ -28,12 +28,12 @@ def inception_network():
                                 kernel_initializer=lay_init)
     conv3 = conv_lay3(conv2)
     maxpooling2D2 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding='same')
+                                          padding='same')
     maxpool2 = maxpooling2D2(conv3)
     block1 = inception_block(maxpool2, [64, 96, 128, 16, 32, 32])
     block2 = inception_block(block1, [128, 128, 192, 32, 96, 64])
     maxpooling2D3 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding='same')
+                                          padding='same')
     maxpool3 = maxpooling2D3(block2)
     block3 = inception_block(maxpool3, [192, 96, 208, 16, 48, 64])
     block4 = inception_block(block3, [160, 112, 224, 24, 64, 64])
@@ -41,16 +41,17 @@ def inception_network():
     block6 = inception_block(block5, [112, 144, 288, 32, 64, 64])
     block7 = inception_block(block6, [256, 160, 320, 32, 128, 128])
     maxpooling2D4 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
-                                      padding='same')
+                                          padding='same')
     maxpool4 = maxpooling2D4(block7)
     block8 = inception_block(maxpool4, [256, 160, 320, 32, 128, 128])
     block9 = inception_block(block8, [384, 192, 384, 48, 128, 128])
-    avergepooling2D= K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))
+    avergepooling2D = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                                strides=(1, 1))
     avgpool = averagepooling2D(block9)
     droper = K.layers.Dropout(0.4)
     dropout = droper(avgpool)
     denser = K.layers.Dense(units=1000, activation='softmax',
                             kernel_initializer=lay_init)
-    dense = denserdropout)
+    dense = denser(dropout)
     model = K.models.Model(inputs=X, outputs=dense)
     return model
