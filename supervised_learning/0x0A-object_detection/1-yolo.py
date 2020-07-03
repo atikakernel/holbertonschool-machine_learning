102a884600052b0fe925ac9c8ddb19475a86a81a
Diego <777@holbertonschool.com>
2020-07-03 16:07:08 -0500

    hd

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -23,49 +23,30 @@ class Yolo():
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def _sigmoid(self, x):
-        """
-        Perform sigmoid function of a vector.
-        """
-        return 1. / (1. + np.exp(-x))
-
     def process_outputs(self, outputs, image_size):
+        """Process Darknet outputs
         """
-        Process Outputs tuple of (boxes, box_confidences, box_class_probs)
-        """
-
-        boxes, box_confidences, box_class_probs = [], [], []
-
-        for i in range(len(outputs)):
-            ih, iw = image_size
-            t_xy, t_wh, objectness, classes = np.split(outputs[i], (2, 4, 5),
-                                                       axis=-1)
-
-            box_confidences.append(_sigmoid(objectness))
-            box_class_probs.append(_sigmoid(classes))
-
-            grid_size = np.shape(outputs[i])[1]
-            C_xy = np.meshgrid(range(grid_size), range(grid_size))
-            C_xy = np.stack(C_xy, axis=-1)
-
-            C_xy = np.expand_dims(C_xy, axis=2)
-            b_xy = _sigmoid(t_xy) + C_xy
-
-            b_xy = b_xy / grid_size
-
-            inp = self.model.input_shape[1:3]
-            b_wh = (np.exp(t_wh) / inp) * self.anchors[i]
-
-            bx = b_xy[:, :, :, :1]
-            by = b_xy[:, :, :, 1:2]
-            bw = b_wh[:, :, :, :1]
-            bh = b_wh[:, :, :, 1:2]
-
-            x1 = (bx - bw / 2) * image_size[1]
-            y1 = (by - bh / 2) * image_size[0]
-            x2 = (bx + bw / 2) * image_size[1]
-            y2 = (by + bh / 2) * image_size[0]
-
-            boxes.append(np.concatenate([x1, y1, x2, y2], axis=-1))
-
+        boxes = [output[:, :, :, 0:4] for output in outputs]
+        for oidx, output in enumerate(boxes):
+            for y in range(output.shape[0]):
+                for x in range(output.shape[1]):
+                    centery = ((1 / (1 + np.exp(-output[y, x, :, 1])) + y)
+                               / output.shape[0] * image_size[0])
+                    centerx = ((1 / (1 + np.exp(-output[y, x, :, 0])) + x)
+                               / output.shape[1] * image_size[1])
+                    prior_resizes = self.anchors[oidx].astype(float)
+                    prior_resizes[:, 0] *= (np.exp(output[y, x, :, 2])
+                                            / 2 * image_size[1] /
+                                            self.model.input.shape[1].value)
+                    prior_resizes[:, 1] *= (np.exp(output[y, x, :, 3])
+                                            / 2 * image_size[0] /
+                                            self.model.input.shape[2].value)
+                    output[y, x, :, 0] = centerx - prior_resizes[:, 0]
+                    output[y, x, :, 1] = centery - prior_resizes[:, 1]
+                    output[y, x, :, 2] = centerx + prior_resizes[:, 0]
+                    output[y, x, :, 3] = centery + prior_resizes[:, 1]
+        box_confidences = [1 / (1 + np.exp(-output[:, :, :, 4, np.newaxis]))
+                           for output in outputs]
+        box_class_probs = [1 / (1 + np.exp(-output[:, :, :, 5:]))
+                           for output in outputs]
         return boxes, box_confidences, box_class_probs
