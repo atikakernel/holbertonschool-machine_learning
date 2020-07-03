fccc230d4dfdab092dcf3f136799ae09e1393ab2
Diego <777@holbertonschool.com>
2020-07-03 16:43:40 -0500

    asd

--- supervised_learning/0x0A-object_detection/4-yolo.py
+++ supervised_learning/0x0A-object_detection/4-yolo.py
@@ -120,44 +120,10 @@ class Yolo():
 
         return boxes_predic, classes_predic, scores_predic
 
-    def nms(self, bc, thresh, scores):
-        """
-        Function that computes the index
-        Sorted index score for non max supression
-        """
-        x1 = bc[:, 0]
-        y1 = bc[:, 1]
-        x2 = bc[:, 2]
-        y2 = bc[:, 3]
-
-        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
-        order = scores.argsort()[::-1]
-
-        keep = []
-        while order.size > 0:
-            i = order[0]
-            keep.append(i)
-            xx1 = np.maximum(x1[i], x1[order[1:]])
-            yy1 = np.maximum(y1[i], y1[order[1:]])
-            xx2 = np.minimum(x2[i], x2[order[1:]])
-            yy2 = np.minimum(y2[i], y2[order[1:]])
-
-            w = np.maximum(0.0, xx2 - xx1 + 1)
-            h = np.maximum(0.0, yy2 - yy1 + 1)
-            inter = w * h
-            ovr = inter / (areas[i] + areas[order[1:]] - inter)
-            inds = np.where(ovr <= thresh)[0]
-            order = order[inds + 1]
-        return keep
-
     @staticmethod
     def load_images(folder_path):
-        """
-        Function to load images using cv2
-        """
-        images = []
-        images_paths = glob.glob(folder_path + '/*', recursive=False)
-        for i in images_paths:
-            image = cv2.imread(i)
-            images.append(image)
-        return images, images_paths
+        """ This method load an image """
+        images = [cv2.imread(file)
+                  for file in glob.glob(folder_path + '/*.jpg')]
+        paths = [file for file in glob.glob(folder_path + '/*.jpg')]
+        return images, paths
