daaa6b0736a1895e8f091133fcd2e192ab18325b
Diego <777@holbertonschool.com>
2020-07-29 19:49:48 -0500

    fix1

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def pca(X, var=0.95):
+def pca(X, ndim):
     """weights matrix, W, that maintains var fraction of X‘s
              original variance. W is a ndarray (d, nd)
              nd is the new dimensionality o the transformed X
